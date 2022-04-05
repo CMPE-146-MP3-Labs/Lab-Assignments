f82f0249e41cd23d7ed0d9084d83cc8de4ad21bd
aaron837 <aaron.rice@sjsu.edu>
2022-04-04 17:53:26 -0700

    Template Modified

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -9,8 +9,8 @@
 #include "sj2_cli.h"
 
 // 'static' to make these functions 'private' to this file
-static void create_producer_task(void);
-static void create_consumer_task(void);
+static void create_producer_task(int part);
+static void create_consumer_task(int part);
 static void create_watchdog_task(void);
 static void producer_task(void *params);
 static void consumer_task(void *params);
@@ -19,8 +19,9 @@ static void watchdog_task(void *params);
 void write_file_using_fatfs_pi(void);
 
 int main(void) {
-  create_producer_task();
-  create_consumer_task();
+  int part = 0;
+  create_producer_task(part);
+  create_consumer_task(part);
   create_watchdog_task();
 
   puts("Starting RTOS");
@@ -29,56 +30,83 @@ int main(void) {
   return 0;
 }
 
-static void create_producer_task(void) {
-  xTaskCreate(producer_task, "Producer", (512U * 8) / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
+static void create_producer_task(part) {
+  xTaskCreate(producer_task, "Producer", (512U * 8) / sizeof(void *), part, PRIORITY_MEDIUM, NULL);
 }
 
 
-static void create_consumer_task(void) {
-  xTaskCreate(consumer_task, "Consumer", (512U * 8) / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
+static void create_consumer_task(part) {
+  xTaskCreate(consumer_task, "Consumer", (512U * 8) / sizeof(void *), part, PRIORITY_MEDIUM, NULL);
 }
 
-static void create_consumer_task(void) {
+static void create_watchdog_task() {
   xTaskCreate(watchdog_task, "Watchdog", (512U * 8) / sizeof(void *), NULL, PRIORITY_HIGH, NULL);
 }
 
 
-/*
-Create a producer task that reads a sensor value every 1ms.
-- The sensor can be any input type, such as a light sensor, or an acceleration sensor
-- After collecting 100 samples (after 100ms), compute the average
-- Write average value every 100ms (avg. of 100 samples) to the sensor queue
-- Use medium priority for this task
-*/
+
 static void producer_task(void *params) {
-    while(1) { // Assume 100ms loop - vTaskDelay(100)
-    // Sample code:
-    // 1. get_sensor_value()
-    // 2. xQueueSend(&handle, &sensor_value, 0);
-    // 3. xEventGroupSetBits(checkin)
-    // 4. vTaskDelay(100)
+  if (params = 0)
+    while(1) {
+    if (params = 0){
+      /*
+      Create a producer task that reads a sensor value every 1ms.
+      - The sensor can be any input type, such as a light sensor, or an acceleration sensor
+      - After collecting 100 samples (after 100ms), compute the average
+      - Write average value every 100ms (avg. of 100 samples) to the sensor queue
+      - Use medium priority for this task
+      */
+    }
+
+    else if (params = 1){
+      // Assume 100ms loop - vTaskDelay(100)
+      // Sample code:
+      // 1. get_sensor_value()
+      // 2. xQueueSend(&handle, &sensor_value, 0);
+      // 3. xEventGroupSetBits(checkin)
+      // 4. vTaskDelay(100)
+    }
+
+    else{
+
+    }
+
   }
 
 }
-/*
-Create a consumer task that pulls the data off the sensor queue
-- Use infinite timeout value during xQueueReceive API
-- Open a file (i.e.: sensor.txt), and append the data to an output file on the SD card
-- Save the data in this format: sprintf("%i, %i\n", time, light)"
-- Note that you can get the time using xTaskGetTickCount()
-- The sensor type is your choice (such as light or acceleration)
-- Note that if you write and close a file every 100ms, it may be very inefficient, so try to come up with a better method such that the file is only written once a second or so...
-- Also, note that periodically you may have to "flush" the file (or close it) otherwise data on the SD card may be cached and the file may not get written
-- Use medium priority for this task
-*/
+
 static void consumer_task(void *params) {
-    while(1) { // Assume 100ms loop
-    // No need to use vTaskDelay() because the consumer will consume as fast as production rate
-    // because we should block on xQueueReceive(&handle, &item, portMAX_DELAY);
-    // Sample code:
-    // 1. xQueueReceive(&handle, &sensor_value, portMAX_DELAY); // Wait forever for an item
-    // 2. xEventGroupSetBits(checkin)
+  if (params = 0)
+    while(1) {
+    if (params = 0){
+      /*
+      Create a consumer task that pulls the data off the sensor queue
+      - Use infinite timeout value during xQueueReceive API
+      - Open a file (i.e.: sensor.txt), and append the data to an output file on the SD card
+      - Save the data in this format: sprintf("%i, %i\n", time, light)"
+      - Note that you can get the time using xTaskGetTickCount()
+      - The sensor type is your choice (such as light or acceleration)
+      - Note that if you write and close a file every 100ms, it may be very inefficient, so try to come up with a better method such that the file is only written once a second or so...
+      - Also, note that periodically you may have to "flush" the file (or close it) otherwise data on the SD card may be cached and the file may not get written
+      - Use medium priority for this task
+      */
+    }  
+
+    else if (params = 1){
+      // Assume 100ms loop
+      // No need to use vTaskDelay() because the consumer will consume as fast as production rate
+      // because we should block on xQueueReceive(&handle, &item, portMAX_DELAY);
+      // Sample code:
+      // 1. xQueueReceive(&handle, &sensor_value, portMAX_DELAY); // Wait forever for an item
+      // 2. xEventGroupSetBits(checkin)
+    }
+
+    else{
+
+    }
+
   }
+
 }
 
 /*
