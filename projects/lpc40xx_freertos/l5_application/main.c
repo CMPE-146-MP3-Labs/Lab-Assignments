6ba2140f99898b3dc6373756b1be5e365041d000
aaron837 <aaron.rice@sjsu.edu>
2022-04-04 17:27:32 -0700

    Template Created

--- projects/lpc40xx_freertos/l5_application/main.c
+++ projects/lpc40xx_freertos/l5_application/main.c
@@ -2,26 +2,26 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-
+#include "ff.h"
 #include "board_io.h"
 #include "common_macros.h"
 #include "periodic_scheduler.h"
 #include "sj2_cli.h"
 
 // 'static' to make these functions 'private' to this file
-static void create_blinky_tasks(void);
-static void create_uart_task(void);
-static void blink_task(void *params);
-static void uart_task(void *params);
+static void create_producer_task(void);
+static void create_consumer_task(void);
+static void create_watchdog_task(void);
+static void producer_task(void *params);
+static void consumer_task(void *params);
+static void watchdog_task(void *params);
+
+void write_file_using_fatfs_pi(void);
 
 int main(void) {
-  create_blinky_tasks();
-  create_uart_task();
-
-  // If you have the ESP32 wifi module soldered on the board, you can try uncommenting this code
-  // See esp32/README.md for more details
-  // uart3_init();                                                                     // Also include:  uart3_init.h
-  // xTaskCreate(esp32_tcp_hello_world_task, "uart3", 1000, NULL, PRIORITY_LOW, NULL); // Include esp32_task.h
+  create_producer_task();
+  create_consumer_task();
+  create_watchdog_task();
 
   puts("Starting RTOS");
   vTaskStartScheduler(); // This function never returns unless RTOS scheduler runs out of memory and fails
@@ -29,83 +29,95 @@ int main(void) {
   return 0;
 }
 
-static void create_blinky_tasks(void) {
-  /**
-   * Use '#if (1)' if you wish to observe how two tasks can blink LEDs
-   * Use '#if (0)' if you wish to use the 'periodic_scheduler.h' that will spawn 4 periodic tasks, one for each LED
-   */
-#if (1)
-  // These variables should not go out of scope because the 'blink_task' will reference this memory
-  static gpio_s led0, led1;
-
-  // If you wish to avoid malloc, use xTaskCreateStatic() in place of xTaskCreate()
-  static StackType_t led0_task_stack[512 / sizeof(StackType_t)];
-  static StackType_t led1_task_stack[512 / sizeof(StackType_t)];
-  static StaticTask_t led0_task_struct;
-  static StaticTask_t led1_task_struct;
-
-  led0 = board_io__get_led0();
-  led1 = board_io__get_led1();
-
-  xTaskCreateStatic(blink_task, "led0", ARRAY_SIZE(led0_task_stack), (void *)&led0, PRIORITY_LOW, led0_task_stack,
-                    &led0_task_struct);
-  xTaskCreateStatic(blink_task, "led1", ARRAY_SIZE(led1_task_stack), (void *)&led1, PRIORITY_LOW, led1_task_stack,
-                    &led1_task_struct);
-#else
-  periodic_scheduler__initialize();
-  UNUSED(blink_task);
-#endif
+static void create_producer_task(void) {
+  xTaskCreateStatic(producer_task, "Producer", (512U * 8) / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
 }
 
-static void create_uart_task(void) {
-  // It is advised to either run the uart_task, or the SJ2 command-line (CLI), but not both
-  // Change '#if (0)' to '#if (1)' and vice versa to try it out
-#if (0)
-  // printf() takes more stack space, size this tasks' stack higher
-  xTaskCreate(uart_task, "uart", (512U * 8) / sizeof(void *), NULL, PRIORITY_LOW, NULL);
-#else
-  sj2_cli__init();
-  UNUSED(uart_task); // uart_task is un-used in if we are doing cli init()
-#endif
+
+static void create_consumer_task(void) {
+  xTaskCreate(consumer_task, "Consumer", (512U * 8) / sizeof(void *), NULL, PRIORITY_MEDIUM, NULL);
 }
 
-static void blink_task(void *params) {
-  const gpio_s led = *((gpio_s *)params); // Parameter was input while calling xTaskCreate()
+static void create_consumer_task(void) {
+  xTaskCreate(watchdog_task, "Watchdog", (512U * 8) / sizeof(void *), NULL, PRIORITY_HIGH, NULL);
+}
 
-  // Warning: This task starts with very minimal stack, so do not use printf() API here to avoid stack overflow
-  while (true) {
-    gpio__toggle(led);
-    vTaskDelay(500);
+
+/*
+Create a producer task that reads a sensor value every 1ms.
+- The sensor can be any input type, such as a light sensor, or an acceleration sensor
+- After collecting 100 samples (after 100ms), compute the average
+- Write average value every 100ms (avg. of 100 samples) to the sensor queue
+- Use medium priority for this task
+*/
+static void producer_task(void *params) {
+    while(1) { // Assume 100ms loop - vTaskDelay(100)
+    // Sample code:
+    // 1. get_sensor_value()
+    // 2. xQueueSend(&handle, &sensor_value, 0);
+    // 3. xEventGroupSetBits(checkin)
+    // 4. vTaskDelay(100)
+  }
+
+}
+/*
+Create a consumer task that pulls the data off the sensor queue
+- Use infinite timeout value during xQueueReceive API
+- Open a file (i.e.: sensor.txt), and append the data to an output file on the SD card
+- Save the data in this format: sprintf("%i, %i\n", time, light)"
+- Note that you can get the time using xTaskGetTickCount()
+- The sensor type is your choice (such as light or acceleration)
+- Note that if you write and close a file every 100ms, it may be very inefficient, so try to come up with a better method such that the file is only written once a second or so...
+- Also, note that periodically you may have to "flush" the file (or close it) otherwise data on the SD card may be cached and the file may not get written
+- Use medium priority for this task
+*/
+static void consumer_task(void *params) {
+    while(1) { // Assume 100ms loop
+    // No need to use vTaskDelay() because the consumer will consume as fast as production rate
+    // because we should block on xQueueReceive(&handle, &item, portMAX_DELAY);
+    // Sample code:
+    // 1. xQueueReceive(&handle, &sensor_value, portMAX_DELAY); // Wait forever for an item
+    // 2. xEventGroupSetBits(checkin)
   }
 }
 
-// This sends periodic messages over printf() which uses system_calls.c to send them to UART0
-static void uart_task(void *params) {
-  TickType_t previous_tick = 0;
-  TickType_t ticks = 0;
+/*
+Create a watchdog task that monitors the operation of the two tasks.
+- Use high priority for this task.
+- Use a task delay of 1 second, and wait for all the bits to set. If there are two tasks, wait for bit1, and bit2 etc.
+- If you fail to detect the bits are set, that means that the other tasks did not reach the end of the loop.
+- Print a message when the Watchdog task is able to verify the check-in of other tasks
+- Print an error message clearly indicating which task failed to check-in with the RTOS Event Groups API
+*/
+void watchdog_task(void *params) {
+  while(1) {
+    // ...
+    // vTaskDelay(200);
+    // We either should vTaskDelay, but for better robustness, we should
+    // block on xEventGroupWaitBits() for slightly more than 100ms because
+    // of the expected production rate of the producer() task and its check-in
+    
+    if (xEventGroupWaitBits(...)) { // TODO
+      // TODO
+    }
+  }
+}
 
-  while (true) {
-    // This loop will repeat at precise task delay, even if the logic below takes variable amount of ticks
-    vTaskDelayUntil(&previous_tick, 2000);
+void write_file_using_fatfs_pi(void) {
+  const char *filename = "file.txt";
+  FIL file; // File handle
+  UINT bytes_written = 0;
+  FRESULT result = f_open(&file, filename, (FA_WRITE | FA_CREATE_ALWAYS));
 
-    /* Calls to fprintf(stderr, ...) uses polled UART driver, so this entire output will be fully
-     * sent out before this function returns. See system_calls.c for actual implementation.
-     *
-     * Use this style print for:
-     *  - Interrupts because you cannot use printf() inside an ISR
-     *    This is because regular printf() leads down to xQueueSend() that might block
-     *    but you cannot block inside an ISR hence the system might crash
-     *  - During debugging in case system crashes before all output of printf() is sent
-     */
-    ticks = xTaskGetTickCount();
-    fprintf(stderr, "%u: This is a polled version of printf used for debugging ... finished in", (unsigned)ticks);
-    fprintf(stderr, " %lu ticks\n", (xTaskGetTickCount() - ticks));
-
-    /* This deposits data to an outgoing queue and doesn't block the CPU
-     * Data will be sent later, but this function would return earlier
-     */
-    ticks = xTaskGetTickCount();
-    printf("This is a more efficient printf ... finished in");
-    printf(" %lu ticks\n\n", (xTaskGetTickCount() - ticks));
+  if (FR_OK == result) {
+    char string[64];
+    sprintf(string, "Value,%i\n", 123);
+    if (FR_OK == f_write(&file, string, strlen(string), &bytes_written)) {
+    } else {
+      printf("ERROR: Failed to write data to file\n");
+    }
+    f_close(&file);
+  } else {
+    printf("ERROR: Failed to open: %s\n", filename);
   }
 }
